e41fe8a193e294238bd9dc01743cff2f129911e0
yeonjuLee <yeonju509@gmail.com>
2024-10-07 03:33:26 +0900

    백트래킹 문제풀이 추가(푼 버전, 복습x)

---
+++ 삼성대비/0x0D_simulation/12100_2048(EASY).cpp
@@ -0,0 +1,96 @@
+// 틀린 이유
+// stack에서 2 4 4 8 4 4 -> 2 8 8 8 이런식으로 되어야하는데
+// 잘못 구성해서
+// 8(합) 8(합) 2 8 이런식으로 구현됐을 듯
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int board[22][22];
+
+int maxi = 0;
+
+void rotate(int rot){ // board 회전
+    int tmp[22][22];
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            if (rot == 0) tmp[i][j] = board[i][j];
+            else if (rot == 1) tmp[j][N - 1 - i] = board[i][j];
+            else if (rot == 2) tmp[N - 1 - i][N - 1 - j] = board[i][j];
+            else if (rot == 3) tmp[N - 1 - j][i] = board[i][j];
+        }
+    }
+    memcpy(board, tmp, sizeof(board));
+}
+
+void play(){
+    int ans[22][22];
+    for (int i = 0; i < N; i++) fill(ans[i], ans[i] + N, 0);
+    
+    for (int i = 0; i < N; i++){
+        stack<int> num;
+        int idx = 0;
+        for (int j = 0; j < N; j++){
+            if (board[i][j] != 0){
+                if (!num.empty()){
+                    if (num.top() == board[i][j]){
+                        ans[i][idx++] = num.top() * 2;
+                        num.pop();
+                    }
+                    else {
+                        ans[i][idx++] = num.top();
+                        num.pop();
+                        num.push(board[i][j]);
+                    }
+                }
+                // 스택이 빈다면, top, pop 할 수 없음
+                else {
+                    num.push(board[i][j]);
+                }
+            }
+        }
+        // 맨 마지막 원소가 남아 있다면
+        if (!num.empty()){
+            ans[i][idx] = num.top(); num.pop();
+        }
+    }
+    // board update
+    memcpy(board, ans, sizeof(ans));
+}
+
+void dfs(int dep){
+    if (dep == 5){
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < N; j++){
+                maxi = max(maxi, board[i][j]);
+                // cout << board[i][j] << " ";
+            }
+            // cout << endl;
+        }
+        return;
+    }
+    for (int rot = 0; rot < 4; rot++){
+        int tmp[22][22];
+        memcpy(tmp, board, sizeof(tmp));
+        // 순회
+        rotate(rot);
+        play();
+        dfs(dep + 1);
+        // 복구
+        memcpy(board, tmp, sizeof(board));
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++){
+        fill(board[i], board[i] + N, 0);
+        for (int j = 0; j < N; j++){
+            cin >> board[i][j];
+        }
+    }
+    dfs(0);
+    cout << maxi;
+}
--- 삼성대비/README.md
+++ 삼성대비/README.md
@@ -5,3 +5,6 @@
 - [블로그](https://blog.encrypted.gg/921)
 
 > 기본자료는 바킹독을 참고. 1주일 남았으므로, 코드트리나 백준에 있는 기출문제를 풀 예정.
+
+## 삼성전자 SW 역량테스트 합격 전략
+- [링크](https://www.codetree.ai/blog/%EC%82%BC%EC%84%B1-sw-%EC%97%AD%EB%9F%89%ED%85%8C%EC%8A%A4%ED%8A%B8-%EC%A4%80%EB%B9%84-%EC%8B%A4%EC%A0%84-%EB%AC%B8%EC%A0%9C-%ED%92%80%EC%9D%B4%EB%B6%80%ED%84%B0-%ED%95%A9%EA%B2%A9-%EC%A0%84%EB%9E%B5/)
