9b8aaf31b360a02ddf1a18ea6b3f327699bf9779
yeonjuLee <yeonju509@gmail.com>
2024-10-15 23:38:17 +0900

    [level 2] Title: 가장 큰 수, Time: 46.23 ms, Memory: 13.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+pair<string, string> num_str[100001];
+int N, zeroN;
+
+bool cmp(pair<string, string> &a, pair<string, string> &b){
+    if (a.first > b.first) return 1;
+    return 0;
+}
+
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    
+    string s, snum;
+    int len = numbers.size(), idx = 0;
+    for (int i = 0; i < len; i++){
+        snum = "", s = to_string(numbers[i]), N = s.size();
+        if (s == "0") { zeroN++; continue; }
+        for (int j = 0; j < 5 - N; j++) snum += s; // snum * 3
+        snum = snum.substr(0, 4);
+        num_str[idx++] = {snum, to_string(numbers[i])};
+    }
+    
+    sort(num_str, num_str + idx);
+    while (idx--) answer += num_str[idx].second;
+
+    // 모두 0인 경우, 
+    if (answer == "") answer = "0";
+    else {
+        while (zeroN--) answer += "0";
+    }
+    return answer;
+}
