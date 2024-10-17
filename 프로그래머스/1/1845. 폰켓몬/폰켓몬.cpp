00835541c85934e06df5df737c470bdc39dab182
yeonjuLee <yeonju509@gmail.com>
2024-10-17 13:51:06 +0900

    [level 1] Title: 폰켓몬, Time: 0.90 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int solution(vector<int> nums)
 {
-    unordered_set<int> pocket;
     int answer = 0;
-    for (auto i : nums){
-        pocket.insert(i);
-    }
+    unordered_set<int> pocket(nums.begin(), nums.end());
+    // unordered_set<int> pocket;
+    // for (auto i : nums){
+    //     pocket.insert(i);
+    // }
     answer = min(int(nums.size() / 2), int(pocket.size()));
     return answer;
 }
