896401446904feef5ba98bac8ae66f0cc91b9543
yeonjuLee <yeonju509@gmail.com>
2024-10-30 18:08:19 +0900

    [level 3] Title: 입국심사, Time: 43.44 ms, Memory: 6.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -6,14 +6,13 @@ using namespace std;
 
 long long solution(int n, vector<int> times) {
     long long start = 1, end = (long long)*max_element(times.begin(), times.end()) * (long long)n;
-    long long answer = end;
+    long long answer = (long long)*max_element(times.begin(), times.end()) * (long long)n;;
     
     while (start <= end){
         long long mid = (start + end) / 2;
         long long cnt = 0;
         for (int t : times){
             cnt += (mid / t);
-            if (cnt  >= n) break;
         }
         if (cnt >= n){
             answer = mid;
