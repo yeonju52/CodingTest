86906156bb5cc3f565368bd18f500b67b09c1428
yeonjuLee <yeonju509@gmail.com>
2024-11-04 00:45:24 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -2,21 +2,37 @@
 #include <vector>
 #include <unordered_map>
 #include <iostream>
+
 using namespace std;
 
-string docs = "AEIOU"; // 0 1 2 3 4
-unordered_map<char, int> docs_map;
-int pos[5];
+string alpha = "AEIOU";
+int base[5]; // = {781, 156, 31, 6, 1};
+unordered_map<char, int> book;
+
+void setBase(){
+    int factor = 1;
+    base[4] = 1;
+    for (int i = 4; i >= 0; i--){
+        factor *= 5;
+        base[i - 1] = base[i] + factor;
+    }
+}
+
+void setMap(string alpha){
+    int idx = 0;
+    for (char s : alpha){
+        book.insert({s, idx++});
+    }
+}
 
 int solution(string word) {
-    int answer = 0;
-    for (int i = 0; i < 5; i++) docs_map[docs[i]] = i; // 'A': 0
-    pos[4] = 1; // pos = { 781 156 31 6 1 }
-    for (int i = 3; i >= 0; i--) pos[i] = pos[i + 1] * 5;
-    for (int i = 3; i >= 0; i--) pos[i] += pos[i + 1];
-    int idx =  0;
-    for (auto& s : word) {
-        answer += (docs_map[s] * pos[idx++] + 1);
+    // 사전 준비
+    setBase(); // base 구하기 (781 156 31 6 1)
+    setMap("AEIOU"); // book['A'] = 0, book['B'] = 1, ...
+
+    int answer = 0, idx = 0;
+    for (char s : word){
+        answer += (book[s] * base[idx++] + 1);
     }
     return answer;
 }
