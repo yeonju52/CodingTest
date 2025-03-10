c59de134139184e8acea59e868327092d99ba5ec
yeonjuLee <yeonju509@gmail.com>
2025-03-10 18:48:06 +0900

    [Silver II] Title: 나무 자르기, Time: 144 ms, Memory: 9712 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.cc
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.cc
@@ -1,30 +1,37 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int N;
-long long M;
-long long tree[1000002];
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-int main(){
-    cin >> N >> M;
+  long long N, M, sumM, tree[1000000];
+  cin >> N >> M;
+  for (int i = 0; i < N; i++){
+    cin >> tree[i];
+  }
+
+  long long st = 0, ed = *max_element(tree, tree + N), answer = 0, mid;
+
+  while (st <= ed) {
+    mid = (st + ed) / 2;
+
+    long long sumM = 0;
     for (int i = 0; i < N; i++){
-        cin >> tree[i];
+      if (tree[i] > mid) {
+        sumM += (tree[i] - mid);
+      }
     }
+    if (sumM >= M) {
+      st = mid + 1;
+      answer = mid;
+    }
+    else {
+      ed = mid - 1;
+    }
+  }
 
-    long long st = 0, end = *max_element(tree, tree + N), answer = 0, mid;
-    while(st <= end){
-        mid = (st + end) / 2;
-        long long sumM = 0;
-        for (int i = 0; i < N; i++){
-            sumM += (tree[i] - min(mid, tree[i]));
-        }
-        if (sumM >= M) {
-            st = mid + 1;
-            answer = mid;
-        }
-        else {
-            end = mid - 1;
-        }
-    }
-    cout << answer;
+  cout << answer;
+  return 0;
 }
