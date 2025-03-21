78241584f3c90e4ab29c204f6de187b9df3e62e7
yeonjuLee <yeonju509@gmail.com>
2025-03-21 21:19:14 +0900

    [level 2] Title: 전화번호 목록, Time: 247.73 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,21 +1,18 @@
-#include <string>
-#include <vector>
-#include <unordered_set>
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    unordered_multiset<string> f_book; // (phone_book.begin(), phone_book.end());
-    for (string num : phone_book){
-        string prefix = "";
-        for(char c : num){
-            prefix += c;
-            f_book.insert(prefix);
+    unordered_set<string> books;
+    sort(phone_book.begin(), phone_book.end());
+    for (auto ps : phone_book) {
+        string b = "";
+        for (auto p : ps) {
+            b += p;
+            if (books.find(b) != books.end()) return false;
         }
-    }
-    for (string num : phone_book){
-        f_book.erase(f_book.find(num));
-        if (f_book.find(num) != f_book.end()) return false;
+        books.insert(ps);
+        
     }
     return true;
 }
