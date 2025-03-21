c9a4b80f92fef7d990b3c13f31330322fcfceae9
yeonjuLee <yeonju509@gmail.com>
2025-03-21 18:38:27 +0900

    [level 2] Title: 피로도, Time: 0.86 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,31 +1,25 @@
-#define INF 1000000000
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int N, maxi;
-vector<vector<int>> dun;
+int N;
+int answer = -1;
 
-void comb(int cur, int k, vector<int> &vis){
-    if (cur > N) return;
-    maxi = max(maxi, cur);
-
-    for (int i = 1; i <= N; i++){
-        if (vis[i]) continue;
-        if (k < dun[i - 1][0]) continue;
-        vis[i] = 1;
-        comb(cur + 1, k - dun[i - 1][1], vis);
-        vis[i] = 0;
+void dfs(int cur, vector<int> &vis, vector<vector<int>> &dungeons, int scr, int dep){
+    vis[cur] = 1;
+    answer = max(dep, answer);
+    
+    for(int nxt = 1; nxt < N; nxt++){
+        if (!vis[nxt] && dungeons[nxt - 1][0] <= scr) dfs(nxt, vis, dungeons, scr - dungeons[nxt - 1][1], dep + 1);
     }
+    vis[cur] = 0;
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    int answer = -1;
-    N = dungeons.size();
-    dun = dungeons;
-    vector<int> empty(9, 0);
-    comb(0, k, empty);
-
-    answer = maxi;
+    N = dungeons.size() + 1;
+    vector<int> vis(N, 0);
+    dfs(0, vis, dungeons, k, 0);
+    
     return answer;
 }
