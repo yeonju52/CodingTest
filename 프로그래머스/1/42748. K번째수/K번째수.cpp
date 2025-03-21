98cbc0070def6690e8e6caacf110e33d80d3ff51
yeonjuLee <yeonju509@gmail.com>
2025-03-21 20:03:22 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -6,10 +6,11 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     for (auto cmd : commands) {
         int i = cmd[0] - 1, j = cmd[1] - 1, k = cmd[2] - 1;
-        vector<int> vec;
-        for (; i <= j; i++) {
-            vec.push_back(array[i]);
-        }
+        vector<int> vec(array.begin() + i, array.begin() + j + 1);
+        // vector<int> vec(array.begin() + i, array.begin() + j);
+        // for (; i <= j; i++) {
+        //     vec.push_back(array[i]);
+        // }
         sort(vec.begin(), vec.end());
         answer.push_back(vec[k]);
     }
