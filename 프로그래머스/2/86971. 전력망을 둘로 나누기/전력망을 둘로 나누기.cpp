99b18fa3cdce9f7db128c07afbba9129713ec6a1
yeonjuLee <yeonju509@gmail.com>
2025-03-21 17:57:09 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.14 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.cpp
@@ -1,52 +1,40 @@
-#define MAX 102
-#define cur first
-#define next second
-#include <bits/stdc++.h>
+#include <string>
+#include <cstring>
+#include <vector>
+
 using namespace std;
 
-int N;
-int board[MAX][MAX];
-queue<pair<int, int>> Q;
-int vis[MAX];
+vector<int> graph[101];
+int vis[101];
 
-void print(){
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++)
-            cout << board[i][j];
-        cout << "\n";
-    }
-}
-
-int bfs(int i_cur, int i_next){
-    int cnt = 0;
-    Q.push({i_cur, i_next});
-    vis[i_cur] = 1;
+int dfs(int v, int cnt) {
+    vis[v] = 1;
     
-    while(!Q.empty()){
-        pair<int, int> wire = Q.front(); Q.pop();
-        for (int idx = 0; idx < N; idx++){
-            if (vis[idx] || !board[wire.next][idx]) continue;
-            if (!board[wire.next][idx]) continue;
-            Q.push({wire.next, idx});
-            vis[wire.next] = 1;
-            cnt++;
-        }
+    int len = graph[v].size();
+    for (int i = 0; i < len; i++){
+        if (!vis[graph[v][i]]) cnt += dfs(graph[v][i], 1);
     }
     return cnt;
 }
+
 int solution(int n, vector<vector<int>> wires) {
-    int answer = -1;
-    N = wires.size() + 1; // 송전탑의 개수 = N + 1
-    for (auto& wire : wires){
-        board[wire[0] - 1][wire[1] - 1] = 1;
-        board[wire[1] - 1][wire[0] - 1] = 1;
+    int answer = 111;
+    
+    // 인접 리스트 생성
+    for (auto w : wires) {
+        int v1 = w[0], v2 = w[1];
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
     }
-    int minCnt = MAX;
-    for (auto& wire : wires){
+    
+    // 하나씩 삭제하면서 완전탐색
+    for (auto w : wires) {
         memset(vis, 0, sizeof(vis));
-        int cnt = bfs(wire[0] - 1, wire[1] - 1) + 1;
-        minCnt = min(abs((N - cnt) - cnt), minCnt);
+        vis[w[0]] = 1;
+        vis[w[1]] = 1;
+        
+        int val1 = dfs(w[0], 1), val2 = dfs(w[1], 1);
+        answer = min(abs(val1 - val2), answer);
     }
-    answer = minCnt;
     return answer;
 }
