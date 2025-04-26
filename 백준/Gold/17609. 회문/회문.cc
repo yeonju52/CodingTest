8985b190dbe436a66377843c1f042a82badb63e7
yeonjuLee <yeonju509@gmail.com>
2025-04-26 17:17:21 +0900

    [Gold V] Title: 회문, Time: 12 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.cc
+++ 백준/Gold/17609. 회문/회문.cc
@@ -10,6 +10,7 @@ bool psudoR(string &s, int L, int R) {
     }
     return true;
 }
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -21,17 +22,9 @@ int main() {
         int ans = 0;
         while (L < R) {
             if (s[L] != s[R]) {
-                if (ans == 0) { // 유사회문
-                    if (s[L + 1] == s[R] && s[L] == s[R - 1]) {
-                        if(psudoR(s, L + 1, R)) { L++, ans = 1; break; }
-                        else if (psudoR(s, L, R - 1)) { R--, ans = 1; break; }
-                        else ans = 2; break;
-                    }
-                    else if (s[L + 1] == s[R]) { ans = 1; L++; continue; }
-                    else if (s[L] == s[R - 1]) { ans = 1; R--; continue; }
-                    else { ans = 2; break; }
-                }
-                else { ans = 2; break; }
+                if (psudoR(s, L + 1, R) || psudoR(s, L, R - 1)) ans = 1;
+                else ans = 2;
+                break;
             }
             L++, R--;
         }
