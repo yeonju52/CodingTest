e07269f63a6a702c6e0b9c9fb1940651bfdea0ae
yeonjuLee <yeonju509@gmail.com>
2025-04-26 14:41:25 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,35 +1,25 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, x;
-int arr[100000];
+int a[100000];
 
-int main(){
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        cin >> arr[i];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N; cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
     }
-    cin >> x;
+    int X; cin >> X;
 
-    sort(arr + 0, arr + n);
+    sort(a, a + N);
 
-    int st = 0, ed = n - 1;
-    int result = 0;
-    
-    int temp;
-    while(st < ed){ // 같을 때는 당연히 안 됨
-        temp = arr[st] + arr[ed];
-        if (temp == x){
-            st++; ed--;
-            result++;
-        }
-        else if (temp > x){ // 목표값보다 크다. 줄여야지
-            ed--;
-        }
-        else { //temp < x // 목표값보다 작다. 늘려야된다.
-            st++;
-        }
+    int ans = 0, L = 0, R = N - 1;
+    while(L < R) {
+        int sumA = a[L] + a[R];
+        if (sumA == X) { ans++; R--, L++; }
+        else if (sumA < X) L++;
+        else R--;
     }
-    cout << result;
+    cout << ans;
 }
