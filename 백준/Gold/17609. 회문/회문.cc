8eca4201dfba346f68555eb00b669a222c842a10
yeonjuLee <yeonju509@gmail.com>
2025-04-26 17:15:20 +0900

    [Gold V] Title: 회문, Time: 12 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.cc
+++ 백준/Gold/17609. 회문/회문.cc
@@ -1,52 +1,40 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-string arr[30];
+const int MAX = 30;
 
-//1
-//abxbbxa
-//1
-//abxxbxa
-int flag = 1;
-
-bool checkRecur(int i, int st, int ed) {
-	while (st < ed) { // 등호 빼도 OK, 홀수 일 때는 같을 때 항상 동일하니까
-		if (arr[i][st] == arr[i][ed]) {
-			st++; ed--; // 완전 회전문: flag = 1;
-		}
-		else {
-			if (!flag) { // flag == 0
-				return false;
-			}
-			flag = 0; // 수정 회전문: flag = 0;
-			return checkRecur(i, st + 1, ed) || checkRecur(i, st, ed - 1);
-		}
-	}
-	return true;
+bool psudoR(string &s, int L, int R) {
+    while (L < R) {
+        if (s[L] != s[R]) return false;   
+        L++, R--;
+    }
+    return true;
 }
-
 int main() {
-	int T;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	cin >> T;
-	for (int i = 0; i < T; i++) {
-		cin >> arr[i];
-	}
-
-	for (int i = 0; i < T; i++) {
-		flag = 1;
-		bool isValid = checkRecur(i, 0, arr[i].size()-1);
-		if (isValid) {
-			if (flag) {
-				cout << 0 << endl;
-			}
-			else {
-				cout << 1 << endl;
-			}
-		}
-		else {
-			cout << 2 << endl;
-		}
-	}
+    int N; cin >> N;
+    while(N--) {
+        string s; cin >> s;
+        int L = 0, R = (int)s.size() - 1;
+        int ans = 0;
+        while (L < R) {
+            if (s[L] != s[R]) {
+                if (ans == 0) { // 유사회문
+                    if (s[L + 1] == s[R] && s[L] == s[R - 1]) {
+                        if(psudoR(s, L + 1, R)) { L++, ans = 1; break; }
+                        else if (psudoR(s, L, R - 1)) { R--, ans = 1; break; }
+                        else ans = 2; break;
+                    }
+                    else if (s[L + 1] == s[R]) { ans = 1; L++; continue; }
+                    else if (s[L] == s[R - 1]) { ans = 1; R--; continue; }
+                    else { ans = 2; break; }
+                }
+                else { ans = 2; break; }
+            }
+            L++, R--;
+        }
+        cout << ans << "\n";
+    }
 }
