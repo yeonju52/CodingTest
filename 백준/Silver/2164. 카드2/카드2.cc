255c99a48f0bef4440ba0691c273d9825d2eec07
yeonjuLee <yeonju509@gmail.com>
2025-04-29 11:26:29 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -5,17 +5,17 @@ int N;
 
 int main() {
     cin >> N;
-    deque<int> arr;
+    queue<int> arr;
     for (int i = 1; i <= N; i++) {
-        arr.push_back(i);
+        arr.push(i);
     }
 
     while(arr.size() > 1) {
-        arr.pop_front();
-        arr.push_back(arr.front());
-        arr.pop_front();
+        arr.pop();
+        arr.push(arr.front());
+        arr.pop();
     }
 
-    cout << arr[0] << " ";
+    cout << arr.front();
     return 0;
 }
