b3db1cafaaeaf91e767a814e9766a65125c35c90
yeonjuLee <yeonju509@gmail.com>
2025-09-29 22:30:22 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 11.73 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.cpp
@@ -7,23 +7,29 @@ using namespace std;
 long long solution(int cap, int n, vector<int> deliveries, vector<int> pickups) {
     long long ans = 0;
     
-    int sumD = 0, sumP = 0;
+    while(!deliveries.empty() && deliveries.back() == 0) deliveries.pop_back();
+    while(!pickups.empty() && pickups.back() == 0) pickups.pop_back();
     
-    for (int i = n - 1; i >= 0; i--) {
-        int cnt = 0;
-        sumD += deliveries[i];
-        sumP += pickups[i];
-        
-        while(sumD > 0 || sumP > 0)
-        {
-            sumD -= cap;
-            sumP -= cap;
-            cnt++;
+    while(!deliveries.empty() || !pickups.empty()) {
+        ans += (max(deliveries.size(), pickups.size())) * 2;
+        int sumD = 0, sumP = 0;
+        while(!deliveries.empty()) {
+            sumD += deliveries.back();
+            deliveries.pop_back();
+            if (sumD > cap) {
+                deliveries.push_back(sumD - cap);
+                break;
+            }
         }
-        ans += (i + 1) * cnt * 2;
-    }
-    
-    
+        while(!pickups.empty()) {
+            sumP += pickups.back();
+            pickups.pop_back();
+            if (sumP > cap) {
+                pickups.push_back(sumP - cap);
+                break;
+            }
+        }
+    }    
     
     return ans;
 }
