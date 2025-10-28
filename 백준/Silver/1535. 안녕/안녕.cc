67f9904bf6d8a0b47085eb0c63345eceb2ad08e8
yeonjuLee <yeonju509@gmail.com>
2025-10-28 16:54:33 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	vector<int> HP(N, 0), joy(N, 0);
+	for (int i = 0; i < N; i++) cin >> HP[i];
+	for (int i = 0; i < N; i++) cin >> joy[i];
+
+	vector<int> dp(101, 0);
+	for (int i = 0; i < N; i++) {
+		for (int j = 100; j > HP[i]; j--) {
+			dp[j] = max(dp[j], dp[j - HP[i]] + joy[i]);
+		}
+	}
+	cout << *max_element(dp.begin() + 1, dp.end());
+}
