a52fea1580ce40702ae794523a4b14a3590629c5
yeonjuLee <yeonju509@gmail.com>
2025-10-30 19:13:37 +0900

    [level 4] Title: 도둑질, Time: 10.69 ms, Memory: 43.4 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 
 int solution(vector<int> money) {
-    int answer = 0;
-    vector<vector<int>> dp(2, vector<int>(money.size(), 0));
+    int N = money.size();
+    vector<vector<int>> dp(2, vector<int>(N, 0));
     
     dp[0][0] = money[0];
-    for (int i = 1; i < money.size(); i++) {
-        if (i != money.size() - 1) dp[0][i] = max(dp[0][i - 2] + money[i], dp[0][i - 1]);
+    for (int i = 1; i < N; i++) {
+        if (i != N - 1) dp[0][i] = max(dp[0][i - 2] + money[i], dp[0][i - 1]);
         dp[1][i] = max(dp[1][i - 2] + money[i], dp[1][i - 1]);
     }
-    return max(dp[0][money.size() - 2], dp[1][money.size() - 1]);
+    return max(dp[0][N - 2], dp[1][N - 1]);
 }
